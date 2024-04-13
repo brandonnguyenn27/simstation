751c00cfb9cda2e660d8e0251cf25da4c2522e42
Hruday Prathipati <hrudayprathipati@Hrudays-Air.attlocal.net>
2024-04-13 12:42:04 -0700

    Worked on SimStationFactory and SimulationPanel

--- src/simstation/SimStationFactory.java
+++ src/simstation/SimStationFactory.java
@@ -1,4 +1,49 @@
 package simstation;
 
-public class SimStationFactory {
+import mvc.AppFactory;
+import mvc.Model;
+import mvc.View;
+
+public class SimStationFactory implements AppFactory {
+
+    public Model makeModel() {
+        return new Simulation();
+    }
+
+    public View makeView(Model model) {
+        return new SimulationView(model);
+    }
+
+    public String getTitle() {
+        return "SimStation";
+    }
+
+    public String[] getHelp() {
+        return new String[]{"Help"};
+    }
+
+    public String about() {
+        return "SimStation Simulation";
+    }
+
+    public String[] getEditCommands() {
+        return new String[]{"Start", "Suspend", "Resume", "Stop", "Stats"};
+    }
+
+    @Override
+    public simstation.Command makeEditCommand(Model model, String type, Object source) {
+        if ("Start".equals(type)) {
+            return new StartCommand(model);
+        } else if ("Suspend".equals(type)) {
+            return new SuspendCommand(model);
+        } else if ("Resume".equals(type)) {
+            return new ResumeCommand(model);
+        } else if ("Stop".equals(type)) {
+            return new StopCommand(model);
+        } else if ("Stats".equals(type)) {
+            return new StatsCommand(model);
+        } else {
+            return null;
+        }
+    }
 }
--- src/simstation/SimulationPanel.java
+++ src/simstation/SimulationPanel.java
@@ -1,4 +1,46 @@
 package simstation;
 
-public class SimulationPanel {
+import mvc.*;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SimulationPanel extends AppPanel {
+
+    public SimulationPanel(AppFactory factory) {
+        super(factory);
+        controlPanel.setLayout(new BoxLayout(controlPanel, BoxLayout.Y_AXIS));
+
+        JButton startButton = new JButton("Start");
+        startButton.addActionListener(this);
+        controlPanel.add(startButton);
+
+        JButton suspendButton = new JButton("Suspend");
+        suspendButton.addActionListener(this);
+        controlPanel.add(suspendButton);
+
+        JButton resumeButton = new JButton("Resume");
+        resumeButton.addActionListener(this);
+        controlPanel.add(resumeButton);
+
+        JButton stopButton = new JButton("Stop");
+        stopButton.addActionListener(this);
+        controlPanel.add(stopButton);
+
+        JButton statsButton = new JButton("Stats");
+        statsButton.addActionListener(this);
+        controlPanel.add(statsButton);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+        if ("Start".equals(command)) {
+        } else if ("Suspend".equals(command)) {
+        } else if ("Resume".equals(command)) {
+        } else if ("Stop".equals(command)) {
+        } else if ("Stats".equals(command)) {
+        }
+    }
+
 }

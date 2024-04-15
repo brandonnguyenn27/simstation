e1b53d588aa08a714d8325328a488548f0495c85
CharlesM <charlesvincent.manaois@sjsu.edu>
2024-04-14 23:59:58 -0700

    Got simulation to work

--- src/plague/PlagueFactory.java
+++ src/plague/PlagueFactory.java
@@ -1,11 +1,15 @@
 package plague;
 import mvc.AppFactory;
 import simstation.*;
-
+import mvc.*;
 public class PlagueFactory extends SimStationFactory {
     @Override
     public Simulation makeModel() {
         return new PlagueSimulation();
+    }
+    @Override
+    public PlagueView makeView(Model m) {
+        return new PlagueView(m);
     }
 //    @Override
 //    public Model makeModel() {
--- src/plague/PlagueSimulation.java
+++ src/plague/PlagueSimulation.java
@@ -3,12 +3,45 @@ import simstation.*;
 import mvc.*;
 
 public class PlagueSimulation extends Simulation {
-    public static int VIRULENCE = 50; // % chance of infection
-    public static int RESISTANCE = 2; // % chance of resisting infection
-//    public void populate() {
-//        for(int i = 0; i < 15; i++)
-//            //addAgent(new Drunk());
-//    }
+    public static int VIRULENCE = 50; // % chance of infection 50
+    public static int RESISTANCE = 2; // % chance of resisting infection 2
+    public static final int CHANCE = 100;
+    public void populate() {
+        for(int i = 0; i < 15; i++)
+            addAgent(new Plague());
+        infectAgents(); // Infect random agents in the World
+    }
+    public void infectAgents() {
+        for (Agent agent : getAgents()) {
+            Plague a = (Plague) agent;
+            if (!a.isInfected()) {
+                if (isSusceptible(agent)) {
+                    int infectionChance = Utilities.rng.nextInt(CHANCE);
+                    if (infectionChance < VIRULENCE) {
+                        a.setInfected(true);
+                    }
+                }
+            }
+        }
+    }
+    // This method tries to infect an infected Agent's neighbor.
+    public void infectNeighbor(Plague agent, Plague neighbor) {
+        if(agent.isInfected()) {
+            if(!neighbor.isInfected()) {
+                if (isSusceptible(neighbor)) {
+                    int infectionChance = Utilities.rng.nextInt(CHANCE);
+                    if (infectionChance < VIRULENCE) {
+                        neighbor.setInfected(true);
+                    }
+                }
+            }
+        }
+    }
+    // Method to determine if an agent is susceptible to infection
+    private boolean isSusceptible(Agent agent) {
+        int resistanceChance = Utilities.rng.nextInt(CHANCE);
+        return resistanceChance >= RESISTANCE; // If resistance chance is greater than or equal to resistance factor, agent is susceptible
+    }
 
     public static void main(String[] args) {
         AppPanel panel = new SimulationPanel(new PlagueFactory());
